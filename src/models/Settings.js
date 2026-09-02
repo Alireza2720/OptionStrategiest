2b900c715b729d3dc6b717a5f86661e845275e94
Alireza2720 <alireza.mokhtari2720@gmail.com>
2026-09-03 01:00:55 +0330

    Update Settings.js

--- src/models/Settings.js
+++ src/models/Settings.js
@@ -1,30 +1,12 @@
 "use strict";
 var mongoose = require("mongoose");
 
-var HUNT_STRATEGY_TYPES = ["cc", "mp", "co", "cv", "strangle", "strangleSell",
-  "callspread", "callspreadbear", "putspread", "putspreadbull", "box"];
-
-var HuntStrategyCfgSchema = new mongoose.Schema({
+var CatSchema = new mongoose.Schema({
   shockRate: { type: Number, default: 0.5 },
+  shockFloor: { type: Number, default: 25 },
   profitRate: { type: Number, default: 0.35 },
-  telegramEnabled: { type: Boolean, default: true }
+  profitFloor: { type: Number, default: 10 }
 }, { _id: false });
-
-function defaultHuntStrategies() {
-  var obj = {};
-  HUNT_STRATEGY_TYPES.forEach(function (t) {
-    obj[t] = { shockRate: 0.5, profitRate: 0.35, telegramEnabled: true };
-  });
-  return obj;
-}
-
-var HuntStrategiesSchema = new mongoose.Schema(
-  HUNT_STRATEGY_TYPES.reduce(function (acc, t) {
-    acc[t] = { type: HuntStrategyCfgSchema, default: function () { return { shockRate: 0.5, profitRate: 0.35, telegramEnabled: true }; } };
-    return acc;
-  }, {}),
-  { _id: false }
-);
 
 var SettingsSchema = new mongoose.Schema({
   ownerId: { type: String, default: "default", unique: true, index: true },
@@ -32,18 +14,18 @@ var SettingsSchema = new mongoose.Schema({
   dteFilterMin: { type: String, default: "" },
   dteFilterMax: { type: String, default: "" },
   checkIntervalSec: { type: Number, default: 60 },
-
   huntOnlyBuyable: { type: Boolean, default: true },
   huntTopN: { type: Number, default: 30 },
-  huntTopNUnlimited: { type: Boolean, default: false },
-  huntCooldownMinutes: { type: Number, default: 720 },
-  huntCooldownForever: { type: Boolean, default: false },
-
-  huntStrategies: {
-    type: HuntStrategiesSchema,
-    default: defaultHuntStrategies
+  huntCooldownHours: { type: Number, default: 12 },
+  lastResetDate: { type: String, default: "" },
+  huntCat1: {
+    type: CatSchema,
+    default: function () { return { shockRate: 0.5, shockFloor: 25, profitRate: 0.35, profitFloor: 10 }; }
+  },
+  huntCat2: {
+    type: CatSchema,
+    default: function () { return { shockRate: 0.5, shockFloor: 30, profitRate: 0.7, profitFloor: 25 }; }
   }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Settings", SettingsSchema);
-module.exports.HUNT_STRATEGY_TYPES = HUNT_STRATEGY_TYPES;
